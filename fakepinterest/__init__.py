fad5fd32d8ba86cc8c921a9652f33a29f4f2a23c
kaiohas <kaio.alves123@gmail.com>
2024-09-02 09:24:46 -0300

    alteracao senha colocando encode e decode

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -5,7 +5,7 @@ from flask_bcrypt import Bcrypt
 import os
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = "os.getenv("DATABASE_URL")"
 #"sqlite:///comunidade.db"
 app.config["SECRET_KEY"] = "58fa827f04e0c7f63dec001627a82a8a"
 app.config["UPLOAD_FOLDER"] = "static/fotos_posts"
